bf6cfe0e7b872f8498583d077d581def8ad835e5
Caleb Zulawski <caleb.zulawski@gmail.com>
2020-10-29 02:03:46 -0400

    Use nix crate for safe abstractions

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,57 +1,92 @@
 #[cfg(unix)]
 pub mod posix {
-    use libc::{c_char, c_long};
+    use nix::Result;
+    use std::os::unix::io::RawFd;
 
-    pub fn shm_open_anonymous() -> c_long {
+    fn shm_open_anonymous_posix() -> Result<RawFd> {
+        use nix::{
+            errno::Errno,
+            fcntl::OFlag,
+            sys::{
+                mman::{shm_open, shm_unlink},
+                stat::Mode,
+            },
+            time::{clock_gettime, ClockId},
+            unistd::close,
+            Error,
+        };
+        use std::{ffi::CStr, io::Write};
+        let mut filename = *b"/shm-vrb-XXXX\0";
+        for _ in 0..4 {
+            // add some random characters to the filename
+            let filename = {
+                let time = clock_gettime(ClockId::CLOCK_REALTIME)?;
+                write!(&mut filename[9..], "{:4}", time.tv_nsec() % 10000).unwrap();
+                CStr::from_bytes_with_nul(filename.as_ref()).unwrap()
+            };
+
+            // open the file
+            match shm_open(
+                filename,
+                OFlag::O_RDWR | OFlag::O_CREAT | OFlag::O_EXCL | OFlag::O_NOFOLLOW,
+                Mode::from_bits(0600).unwrap(),
+            ) {
+                Ok(fd) => {
+                    shm_unlink(filename).map_err(|err| {
+                        let _ = close(fd);
+                        err
+                    })?;
+                    return Ok(fd);
+                }
+                Err(Error::Sys(Errno::EEXIST)) => continue,
+                error => {
+                    error?;
+                }
+            }
+        }
+        Err(Errno::EEXIST.into())
+    }
+
+    pub fn shm_open_anonymous() -> Result<RawFd> {
         #[cfg(target_os = "linux")]
-        unsafe {
-            use libc::{syscall, SYS_memfd_create, MFD_CLOEXEC};
-            syscall(
-                SYS_memfd_create,
-                b"/shm-vmtricks\0".as_ptr() as *const c_char,
-                MFD_CLOEXEC,
-            )
+        {
+            use nix::{
+                error::{Errno, Error},
+                sys::memfd::{memfd_create, MemFdCreateFlag::MFD_CLOEXEC},
+            };
+            match memfd_create("shm-vrb", MFD_CLOEXEC) {
+                Err(Error::Sys(Errno::ENOSYS)) => shm_open_anonymous_posix(),
+                value => value,
+            }
         }
 
         #[cfg(target_os = "freebsd")]
-        unsafe {
-            use libc::{shm_open, O_RDWR, SHM_ANON};
-            shm_open(SHM_ANON, O_RDWR, 0)
+        {
+            use libc::SHM_ANON;
+            use nix::{fcntl::OFlag, sys::mman::shm_open};
+            shm_open(SHM_ANON, OFlag::O_RDWR, 0)
         }
 
         #[cfg(not(any(target_os = "linux", target_os = "freebsd")))]
-        unsafe {
-            use libc::{shm_open, shm_unlink, O_CREAT, O_EXCL, O_NOFOLLOW, O_RDWR};
-            use once_cell::sync::Lazy;
-            use std::sync::Mutex;
-            static MUTEX: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
-            const FILENAME: *const c_char = b"/shm-vmtricks\0".as_ptr() as *const c_char;
-            let guard = MUTEX.lock().unwrap();
-            let fd = shm_open(FILENAME, O_RDWR | O_CREAT | O_EXCL | O_NOFOLLOW, 0600) as c_long;
-            core::mem::drop(guard);
-            if fd != -1 {
-                assert!(shm_unlink(FILENAME) != -1);
-            }
-            fd
-        }
+        shm_open_anonymous_posix()
     }
 
     #[cfg(test)]
     mod test {
         #[test]
         fn shm_open_anonymous() {
-            let fd = super::shm_open_anonymous();
+            let fd = super::shm_open_anonymous().unwrap();
             assert!(fd != -1);
-            unsafe { assert!(libc::close(fd as _) != -1) };
+            nix::unistd::close(fd).unwrap();
         }
     }
 }
 
 pub fn allocation_granularity() -> usize {
     #[cfg(unix)]
-    unsafe {
-        use libc::{sysconf, _SC_PAGESIZE};
-        sysconf(_SC_PAGESIZE) as usize
+    {
+        use nix::unistd::{sysconf, SysconfVar};
+        sysconf(SysconfVar::PAGE_SIZE).unwrap().unwrap() as usize
     }
 }
 
@@ -60,7 +95,7 @@ pub fn allocate(iter: impl Iterator<Item = usize> + Clone) -> *mut u8 {
         .clone()
         .fold(0usize, |blocks, segment| blocks + segment);
 
-    let fd = posix::shm_open_anonymous();
+    let fd = posix::shm_open_anonymous().unwrap();
     assert!(fd != -1);
 
     core::ptr::null_mut()
